ba225407affc402cac23b78040eb96299ea1192a
yves <ngenziyves1@gmail.com>
2022-09-03 00:41:50 +0200

    added database configuration

--- src/databaseConfig/connection.js
+++ src/databaseConfig/connection.js
@@ -1,7 +1,11 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
-const sequelize = new Sequelize(process.env.PG_DATABASE_URL,{
+const sequelize = new Sequelize(
+  process.env.PG_DATABASE_HOST,
+  process.env.PG_DATABASE_USER,
+  process.env.PG_DATABASE_PASSWORD, {
+  host: process.env.PG_DATABASE_HOST,
   dialect: "postgres",
   operatorsAliases: false,
 });
